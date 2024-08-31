9c0ebe003997cb4a8bd1f5a7b3e2d23638acb603
Autumn716 <2683488768@qq.com>
2024-08-31 20:10:02 +0800

    2.1服务注册-定义日志的逻辑

---
+++ log/server.go
@@ -0,0 +1,47 @@
+package log
+
+import (
+	"io"
+	stlog "log" //	将标准库log包取个别名,用来区分自己编写的log包
+	"net/http"
+	"os"
+)
+
+var log *stlog.Logger
+
+type fileLog string
+
+func (fl fileLog) Write(data []byte) (int, error) {
+	f, err := os.OpenFile(string(fl), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+
+	return f.Write(data)
+}
+
+func Run(destination string) {
+	log = stlog.New(fileLog(destination), "go", stlog.LstdFlags)
+}
+
+func RegisterHandlers() {
+	http.HandleFunc("/log", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			msg, err := io.ReadAll(r.Body)
+			if err != nil || len(msg) == 0 {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			write(string(msg))
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+	})
+}
+
+func write(message string) {
+	log.Printf("%v\n", message)
+}
