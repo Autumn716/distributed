c860c74ae975d5dc4b282ae0f04192d62c24e93f
Autumn716 <2683488768@qq.com>
2024-09-06 14:07:41 +0800

    2.5服务注册-注册一个服务

--- service/service.go
+++ service/service.go
@@ -2,21 +2,26 @@ package service
 
 import (
 	"context"
+	"distributed/registry"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func Start(ctx context.Context, serviceName, host, port string,
+func Start(ctx context.Context, host, port string,
+	reg registry.Registration,
 	registerHandlersFunc func()) (context.Context, error) {
 	registerHandlersFunc()
-	ctx = startService(ctx, serviceName, host, port)
-
+	ctx = startService(ctx, reg.ServiceName, host, port)
+	err := registry.RegisterService(reg)
+	if err != nil {
+		return ctx, err
+	}
 	return ctx, nil
 
 }
 
-func startService(ctx context.Context, serviceName, host,
+func startService(ctx context.Context, serviceName registry.ServiceName, host,
 	port string) context.Context {
 
 	ctx, cancel := context.WithCancel(ctx)
